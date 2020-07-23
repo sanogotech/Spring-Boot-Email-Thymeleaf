a592fd6e70dc7a7eeb6b4b759e58216d537d879e
Srikanth Nakka <srikanthnakka@gmail.com>
2020-07-23 23:38:24 +0530

    Initial Commit

---
+++ src/main/java/com/devnakka/email/repository/StudentRepository.java
@@ -0,0 +1,12 @@
+package com.devnakka.email.repository;
+
+import com.devnakka.email.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findByName(String name);
+}
